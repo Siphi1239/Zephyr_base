daa4307cd7a4587dc7a2226f6a17948f58454cdf
Philipp Simon <43174698+Siphi1239@users.noreply.github.com>
2024-09-30 19:33:50 +0000

    added folderhandling

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -3,7 +3,7 @@
 west init -l app
 west update
 west zephyr-export
-pip install -r deps/zephyr_sdk/scripts/requirements.txt
+pip install -r deps/zephyr_sdk/scripts/requirements.txt --break-system-packages
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
 history -c
