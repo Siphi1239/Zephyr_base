f5ed5e0058ca3943434e7a535b1c4b8326c80480
Philipp Simon <43174698+Siphi1239@users.noreply.github.com>
2024-10-01 14:00:29 +0000

    updated zephyr dir

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -3,7 +3,7 @@
 west init -l app
 west update
 west zephyr-export
-pip install -r deps/zephyr_sdk/scripts/requirements.txt --break-system-packages
+pip install -r deps/zephyr/scripts/requirements.txt --break-system-packages
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
 history -c
