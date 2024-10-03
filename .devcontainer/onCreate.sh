42c7d83481fa5d87c87e9ce49bf5f63c7f2b3d51
Philipp Simon <43174698+Siphi1239@users.noreply.github.com>
2024-10-03 20:08:51 +0000

    updates build

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -4,6 +4,8 @@ west init -l app
 west update
 west zephyr-export
 pip install -r deps/zephyr/scripts/requirements.txt --break-system-packages
+west blobs fetch hal_espressif
 west completion bash > $HOME/west-completion.bash
 echo 'source $HOME/west-completion.bash' >> $HOME/.bashrc
+echo "export ZEPHYR_SDK_INSTALL_DIR=/zephyr_toolchain/zephyr-sdk-0.16.8" >> $HOME/.bashrc
 history -c
