396a9a15973737f180fccafa1f41a1c133875080
Philipp Simon <43174698+Siphi1239@users.noreply.github.com>
2024-11-28 22:34:25 +0000

    usb + ot cli

--- src/main.c
+++ src/main.c
@@ -1,7 +1,49 @@
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
+
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+
+LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 
 int main(){
-printk("Suba\n");
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	int ret;
+	const struct device *dev;
+	uint32_t dtr = 0U;
+
+	ret = usb_enable(NULL);
+	if (ret != 0 && ret != -EALREADY) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (dev == NULL) {
+		LOG_ERR("Failed to find specific UART device");
+		return 0;
+	}
+
+	LOG_INF("Waiting for host to be ready to communicate");
+
+	/* Data Terminal Ready - check if host is ready to communicate */
+	while (!dtr) {
+		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		if (ret) {
+			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
+				ret);
+			continue;
+		}
+		k_msleep(100);
+	}
+
+	/* Data Carrier Detect Modem - mark connection as established */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
+	/* Data Set Ready - the NCP SoC is ready to communicate */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+#endif
+
+	LOG_INF("Hello World!");
 return 0;
 }
